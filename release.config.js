47e7c1c80dbd665f20abf9f33b0754aa476915a1
erezrokah <erezrokah@users.noreply.github.com>
2020-07-08 15:07:15 +0300

    chore: fix release script

---
+++ release.config.js
@@ -0,0 +1,21 @@
+/*eslint-env node */
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+      },
+    ],
+  ],
+};
